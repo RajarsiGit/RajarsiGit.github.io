881f1fd98cc55d18bb535b6dc6edbbf059b4235c
Rajarsi Saha <rajarsi3997@gmail.com>
2020-09-19 10:13:57 +0530

    commit

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,48 +1,40 @@
 <?php
-
-// ENTER YOUR EMAIL
-$emailTo = "yourmail@example.com";
-
-// ENTER IDENTIFIER
-$emailIdentifier =  "Message sent via contact form from " . $_SERVER["SERVER_NAME"];
-
-
-if($_POST) {
-
-    $name = addslashes(trim($_POST["name"]));
-    $clientEmail = addslashes(trim($_POST["email"]));
-    $message = addslashes(trim($_POST["message"]));
-	$fhp_input = addslashes(trim($_POST["company"]));
-
-    $array = array("nameMessage" => "", "emailMessage" => "", "messageMessage" => "","succesMessage" => "");
-
-    if($name == "") {
-    	$array["nameMessage"] = "x";
+    // ENTER YOUR EMAIL
+    $emailTo = "rajarsi3997@gmail.com";
+    // ENTER IDENTIFIER
+    $emailIdentifier =  "Resume Message";
+    if($_POST) {
+        $name = addslashes(trim($_POST["name"]));
+        $clientEmail = addslashes(trim($_POST["email"]));
+        $message = addslashes(trim($_POST["message"]));
+        $fhp_input = addslashes(trim($_POST["company"]));
+        if(isset($_POST['g-recaptcha-response']))
+            $captcha=$_POST['g-recaptcha-response'];
+        $array = array("nameMessage" => "", "emailMessage" => "", "messageMessage" => "", "succesMessage" => "");
+        if($name == "") {
+            $array["nameMessage"] = "x";
+        }
+        if(!filter_var($clientEmail, FILTER_VALIDATE_EMAIL)) {
+            $array["emailMessage"] = "x";
+        }
+        if($message == "") {
+            $array["messageMessage"] = "x";
+        }
+        if(!$captcha){
+            echo "<script type='text/javascript'>alert('Sorry! Please try again!');</script>";
+            exit;
+        }
+        $response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Le7zM0ZAAAAAAzQtQ8WqsAxg8ijee6o_WL62I2h&response=" . $captcha . "&remoteip="  . $_SERVER['REMOTE_ADDR']), true);
+        if($name != "" && filter_var($clientEmail, FILTER_VALIDATE_EMAIL) && $message != "" && $fhp_input == "" && $response['success'] == true) {
+            $array["succesMessage"] = "";
+            $headers  = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            $headers .= "From: " . $clientEmail . "\r\n";
+            $headers .= "Reply-To: " . $clientEmail;
+            if(!mail($emailTo, $emailIdentifier, $message, $headers)){
+                $array["succesMessage"] = "x";
+            }
+        }
+        echo json_encode($array);
     }
-	
-    if(!filter_var($clientEmail, FILTER_VALIDATE_EMAIL)) {
-        $array["emailMessage"] = "x";
-    }
-	
-    if($message == "") {
-        $array["messageMessage"] = "x";
-    }
-	
-    if($name != "" && filter_var($clientEmail, FILTER_VALIDATE_EMAIL) && $message != "" && $fhp_input == "") {
-		
-		$array["succesMessage"] = "";
-		
-		$headers  = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-		$headers .= "From: " . $name . " <" . $clientEmail .">\r\n";
-		$headers .= "Reply-To: " . $clientEmail;
-		
-		mail($emailTo, $emailIdentifier, $message, $headers);
-		
-    }
-
-    echo json_encode($array);
-
-}
-
 ?>
